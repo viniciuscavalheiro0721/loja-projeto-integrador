45d75ea528c0a4cf3fe1229c821986bcff7ceab3
Igor Brinker <igorbrinker@gmail.com>
2021-06-27 20:53:05 -0300

    [contact.app] contact form added

---
+++ contact/views.py
@@ -0,0 +1,19 @@
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+def contactView(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['Assunto']
+            from_email = form.cleaned_data['E-mail']
+            message = form.cleaned_data['Mensagem']
+            try:
+                send_mail(subject, message, from_email, ['admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+    return render(request, "contact.html", {'form': form})
