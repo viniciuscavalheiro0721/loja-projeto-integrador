22f7e5a2a0463343fa0bef55b4e831d0b57299b8
viniciuscavalheiro0721 <vinicius.cavalheiro0721@unilasalle.edu.br>
2022-10-04 23:05:48 -0300

    Atualizado para Django v4

--- artifacts/urls.py
+++ artifacts/urls.py
@@ -1,22 +1,19 @@
-from django.urls import path
-from django.conf.urls import url
-
-
+from django.urls import include, path, re_path
 
 from . import views
 
 app_name = "artifacts"
 
 urlpatterns = [
-#    path("", views.ArtifactsListView.as_view(), name="list"),
+    #    path("", views.ArtifactsListView.as_view(), name="list"),
     path("<slug:slug>/", views.ArtifactsDetailView.as_view(), name="detail"),
-    url(r'artifacts/add/$',  views.ArtifactsCreateView.as_view(), name='artifacts_add'),
-    path('artifacts/<pk>', views.ArtifactsUpdateView.as_view(), name='artifacts-update'),
+    re_path(r'artifacts/add/$',  views.ArtifactsCreateView.as_view(),
+            name='artifacts_add'),
+    path('artifacts/<pk>', views.ArtifactsUpdateView.as_view(),
+         name='artifacts-update'),
     path('', views.ArtifactsList, name='list'),
     path('addlogic', views.addlogic, name='add'),
     # path('', views.countries_view, name='list'),
     path('helloworld/', views.helloWorld),
 
 ]
-
-
