fc0b4d4664b912045cb02a1c0024a19d1931a0af
viniciuscavalheiro0721 <vinicius.cavalheiro0721@unilasalle.edu.br>
2022-10-18 10:47:33 -0300

    correcao nos models

--- pdv/models.py
+++ pdv/models.py
@@ -1,23 +1,6 @@
-import random
-import string
-from email.policy import default
-from unittest.util import _MAX_LENGTH
 
-from ckeditor.fields import RichTextField
-from django.contrib import admin
 from django.contrib.auth.models import User
 from django.db import models
-from django.urls import reverse, reverse_lazy
-from tinymce.models import HTMLField
-
-# def generateId():
-#     length = 12
-
-# while True:
-#     artifact_id = ''.join(random.choices(string.ascii_uppercase, k=length))
-#     if Artifacts.objects.filter(artifact_id=artifact_id).count() == 0:
-#         break
-# return artifact_idfrom django.core.urlresolvers import reverse
 
 
 class Pdv(models.Model):
